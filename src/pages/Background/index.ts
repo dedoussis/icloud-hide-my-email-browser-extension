a77e51b641c6447ad49e1aee6539d94b77c5fbe2
Dimitrios Dedoussis <dimitrios@dedouss.is>
2023-06-29 23:15:57 +0100

    Fix log-out bug (#35)
    * Fix log-out bug
    
    * Prettify

--- src/pages/Background/index.ts
+++ src/pages/Background/index.ts
@@ -63,7 +63,7 @@ const getClient = async (withTokenValidation = true): Promise<ICloudClient> => {
           title: SIGNED_OUT_CTA_COPY,
           enabled: false,
         })
-        .catch();
+        .catch(console.debug);
     }
   }
   return client;
@@ -106,7 +106,7 @@ browser.runtime.onMessage.addListener(async (message: Message<unknown>) => {
             title: SIGNED_IN_CTA_COPY,
             enabled: true,
           })
-          .catch();
+          .catch(console.debug);
       }
       break;
     case MessageType.GenerateRequest:
@@ -196,7 +196,7 @@ const createContextMenuItem = (): void => {
             title: SIGNED_OUT_CTA_COPY,
             enabled: false,
           })
-          .catch();
+          .catch(console.debug);
         return;
       }
 
@@ -205,7 +205,7 @@ const createContextMenuItem = (): void => {
           title: SIGNED_IN_CTA_COPY,
           enabled: true,
         })
-        .catch();
+        .catch(console.debug);
     }
   );
 };
@@ -279,10 +279,12 @@ browser.contextMenus.onClicked.addListener(async (info, tab) => {
     );
   } catch (e) {
     if (e instanceof ClientAuthenticationError) {
-      browser.contextMenus.update(CONTEXT_MENU_ITEM_ID, {
-        title: SIGNED_OUT_CTA_COPY,
-        enabled: false,
-      });
+      browser.contextMenus
+        .update(CONTEXT_MENU_ITEM_ID, {
+          title: SIGNED_OUT_CTA_COPY,
+          enabled: false,
+        })
+        .catch(console.debug);
 
       sendMessageToTab(
         MessageType.ActiveInputElementWrite,
